135bd1608e9294cf0eedfda8795b16adb7441a25
yujin Na <olim920@naver.com>
2023-02-18 02:32:02 +0900

    결과보여지기 구현중+수정

--- MeAndCountry_TypeTest/src/main/webapp/resources/js/test.js
+++ MeAndCountry_TypeTest/src/main/webapp/resources/js/test.js
@@ -2,8 +2,58 @@
 const start = document.querySelector("#start-page")
 const main = document.querySelector("#main-page"); 
 const viewResult = document.querySelector('#result-page');
-const endPoint = 7; 
+const endPoint = 6; 
 const select = [];
+
+function totalResult(){
+	console.log(select);
+	calResult();
+}
+
+function calResult(){
+	var clickArray = [
+		{name:'한국', value:0, key:0},
+		{name:'중국', value:0, key:1},
+		{name:'일본', value:0, key:2},
+		{name:'인도', value:0, key:3},
+		{name:'태국', value:0, key:4},
+		{name:'영국', value:0, key:5},
+		{name:'프랑스', value:0, key:6},
+		{name:'이탈리아', value:0, key:7},
+		{name: '독일', value:0, key:8},
+		{name:'스위스', value:0, key:9},
+		{name:'미국', value:0, key:10},
+		{name:'캐나다', value:0, key:11},
+		{name:'멕시코', value:0, key:12},
+		{name:'브라질', value:0, key:13},
+		{name:'아르헨티나', value:0, key:14},
+		{name:'이집트', value:0, key:15},
+		{name:'오스트레일리아', value:0, key:16},
+		{name:'뉴질랜드', value:0, key:17},
+	]
+		for(let i = 0;i<endPoint;i++){
+			var target  = list[i].a[select[i]];
+			for(let j = 0;j<target.type.length;j++){
+				for(let k = 0;k<clickArray.length;k++){
+					if(target.type[j]===clickArray[k].name){
+						clickArray[k].value +=1;
+					}
+				}
+			}
+		}
+		var resultArray = clickArray.sort(function(a,b){
+			if(a.value > b.value){
+				return -1;
+			}
+			else if(a.value <b.value){
+				return 1;
+			}
+			return 0;
+		});
+		console.log(resultArray);
+		let resultString = resultArray[0].key;
+		return resultString;
+}
 
 function result(){
 	main.style.webkitAnimation = "fadeOut 0.4s";
@@ -15,11 +65,13 @@ function result(){
 			main.style.display ="none";
 			viewResult.style.display = "block";
 	},450)})	
+	console.log(select);
+	calResult();
 }
  
 
 //질문,선택 createElement, innerHTML로 구현
-function qAnswer(answerText, qIndex){
+function qAnswer(answerText, qIndex,index){
 	var a = document.querySelector('.answer-box');
 	var answer = document.createElement('button'); //answer에 button넣기
 	answer.classList.add('answerList');//answerdml classList에서 클래스값 넣기
@@ -29,14 +81,21 @@ function qAnswer(answerText, qIndex){
 		var btns = document.querySelectorAll('.answerList'); //버튼 값을 클릭하면 전부 사라지고 다음 문제의 버튼 생성
 		for(let i = 0; i<btns.length;i++){
 			btns[i].disabled = true;
+			//btns[i].style.webkitAnimation = "fadeOut 0.4s";
+			//btns[i].style.animation = "fadeOut 0.4s";
+		}
+		setTimeout(() => {			
+		select[qIndex] = index;//몇번 문제,답을 클릭했는지 select배열에 담기
+		for(let i = 0; i<btns.length;i++){
 			btns[i].style.display = 'none';
 		}
 		next(++qIndex);
+		},450)
 		},false);
 }
 	
 function next(qIndex){
-	if(qIndex+1 === endPoint){//테스트 끝나면 결과페이지 호출
+	if(qIndex === endPoint){//테스트 끝나면 결과페이지 호출
 	result();
 	return;
 	}
@@ -45,7 +104,7 @@ function next(qIndex){
 	q.innerHTML = list[qIndex].q; //list 첫 번째 요소에 q를 넣는다.
 	n.innerHTML = list[qIndex].n;
 	for(let i in list[qIndex].a){
-		qAnswer(list[qIndex].a[i].answer,qIndex);
+		qAnswer(list[qIndex].a[i].answer,qIndex,i);
 	}
 	var progress = document.querySelector('.progress-bar');
 	progress.style.width = (100/endPoint) * (qIndex+1) +'%';
